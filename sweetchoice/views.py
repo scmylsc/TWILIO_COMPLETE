4aa87f1e10266023effe15eaa4188ead513e8516
scmylsc <124163547+scmylsc@users.noreply.github.com>
2023-03-09 15:33:20 +0530

    Update views.py

--- sweetchoice/views.py
+++ sweetchoice/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import os
 from django.http import HttpResponse
 from twilio.rest import Client
+
 def index(request):
 	return render(request,'index.html')
 
@@ -25,10 +26,52 @@ def twilio(request):
 	main_number = int(request.POST["NUMBER_FIELD"])
 
 	main_message = message
-	account_sid = "ACdd79ce84a0c87af94f772a39e9d03f07"
-	auth_token = "a9bbf89474da3742d24638de551b9fc9"
+	account_sid = a
+	auth_token = b
 	client = Client(auth_token,account_sid)
 	client.message.create(to = main_number,
 		       from_ = "",
 			   body = main_message)
 	return render(request,"index.html")
+
+
+class create_account:
+	def __init__(user_name,pass_word):
+		global username
+		global password
+		username = user_name
+		password = pass_word
+	def create_file():
+		fi = open("username.txt",'w')
+		fi.write(username)
+		fi.close()
+	def create_file2():
+		fi = open("password.txt",'w')
+		fi.write(password)
+		fi.close()
+	def read_data():
+		global main_user
+		global main_pass
+		fi = open("username.txt",'r')
+		main_user = fi.read()
+		fi = open("password.txt",'r')
+		main_pass = fi.read()
+	def validate():
+		a = "WRONG USERNAME"
+		b = "WRONG PASSWORD"
+
+		if(username == main_user):
+			if( password == main_pass):
+				return render(request,"user_panel.html")
+			else:
+				print(b)
+		else:
+			print(a)
+
+		
+
+
+if __name__ == "__main__":
+	username = request.POST("username")
+	password = request.POST("password")
+	write_username.validate(username,password)
